022442975d20f2dffe688e4500580f6275a4bd0a
Stanislav Akeldov <akeldovstas@gmail.com>
2026-02-08 20:47:16 +0300

    Added ICovariantList interface

---
+++ src/Akeldov.Collections.Generic/ICovariantList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Akeldov.Collections
+{
+    public interface ICovariantList<out T> : IEnumerable<T>
+    {
+        int Count { get; }
+
+        T Get(int index);
+
+        void Shuffle();
+
+        void Reverse();
+
+        ICovariantList<T> Copy();
+    }
+}
